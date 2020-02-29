174b2abc65b572d04d8378d0b6489c7d78220c19
Gr99vn <duc.trantrung99@gmail.com>
2020-02-29 23:13:01 +0700

    Improve readable code: new method

--- FileOperation.py
+++ FileOperation.py
@@ -1,37 +1,40 @@
 import re
-def doConvert(fileIn, fileOut):
-    fin = open(fileIn,"r",errors='ignore')
-    fout = open(fileOut,"w")
-    # content = fin.read()
-    # print(content)
-    newIndex = 0
-    oldIndex = 1
-    numberSub = 1 
-    for line in fin:
-        if newIndex == 0:
-            newIndex += 1
-            continue 
-        if oldIndex + 3 == newIndex:
-            numberSub += 1
-            oldIndex = newIndex
-        if line.isspace():
-            fout.write(str(numberSub) + '\n')
-        if oldIndex + 1 == newIndex:
-            confirm = re.search(' --> ', line)
-            if confirm:
-                strIn = line.split(' --> ')
-                match = re.sub(r'[.]{1}', ',', strIn[0])
-                sout = '00:' + match
-                strOut = sout
-                match = re.sub(r'[.]{1}', ',', strIn[1])
-                sout = '00:' + match
-                strOut += ' --> ' + sout
-                fout.write(strOut)
-        if oldIndex + 2 == newIndex:
-            fout.write(line + '\n')
-        newIndex += 1
-    fin.close()
-    fout.close()
+
 def makeFileName(fileBase):
     newFName = re.sub(r'.vtt$', '', fileBase) 
     return  newFName + '.srt'
+def convertTime(string):
+    confirm = re.search(' --> ', string)
+    if confirm:
+        strIn = string.split(' --> ')
+        match = re.sub(r'[.]{1}', ',', strIn[0])
+        sout = '00:' + match
+        strOut = sout
+        match = re.sub(r'[.]{1}', ',', strIn[1])
+        sout = '00:' + match
+        strOut += ' --> ' + sout
+        return strOut
+    else:
+        return ''
+
+def doConvert(fileIn, fileOut):
+    fin = open(fileIn, "r", errors='ignore')
+    fout = open(fileOut, "w")
+    numberSub = 0
+    inc = 0
+    listline = []
+    for line in fin:
+        if numberSub == 0:
+            numberSub = 1
+            continue
+        listline.append(line)
+        inc += 1
+        if inc == 3:
+            fout.write(str(numberSub) + '\n')
+            fout.write(convertTime(listline[1]))
+            fout.write(listline[2] + '\n')
+            inc = 0
+            numberSub += 1
+            listline.clear()
+    fin.close()
+    fout.close()
